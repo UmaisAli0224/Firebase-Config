10bf867b28e1eb77588f93f22f8d30dfde10314d
UmaisAli0224 <142864418+UmaisAli0224@users.noreply.github.com>
2023-08-23 21:07:41 +0500

    Create Firebase-Config.js

---
+++ Firebase-Config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app-name.firebaseapp.com",
+  projectId: "your-app-name",
+  storageBucket: "your-app-name.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
